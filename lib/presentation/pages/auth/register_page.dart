cf0a803bf4f615370e0afadb1f5107f02fd66a7e
faustojc <faustojcboko@gmail.com>
2025-02-19 14:38:10 +0800

    feat: Implement initial register page

---
+++ lib/presentation/pages/auth/register_page.dart
@@ -0,0 +1,13 @@
+// TODO: Implement register page
+
+// NOTE: import this material package from flutter as it's our main UI package
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatelessWidget {
+  const RegisterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
