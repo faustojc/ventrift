546ce040cbd59240573bfceadad186085d45165f
faustojc <faustojcboko@gmail.com>
2025-02-19 14:36:07 +0800

    feat: Implement initial login page structure

---
+++ lib/presentation/pages/auth/login_page.dart
@@ -0,0 +1,13 @@
+// TODO: Implement login page
+
+// Note: Import this material package when creating a widget
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
