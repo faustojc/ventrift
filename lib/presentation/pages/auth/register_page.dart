83f5fca4d490b7be6de838c880626a29f62d3097
Kielzu <112738101+Kielzu@users.noreply.github.com>
2025-02-23 03:31:13 +0800

    Registration Initial Files
    - Formulated the registration page
    
    -Added Widgets folder which contains:
    > Registration folder
    - textfield
    - text
    - divider
    - indexes
    - dob_calendar
    - button

--- lib/presentation/pages/auth/register_page.dart
+++ lib/presentation/pages/auth/register_page.dart
@@ -1,13 +1,77 @@
-// TODO: Implement register page
-
-// NOTE: import this material package from flutter as it's our main UI package
 import 'package:flutter/material.dart';
+import '/widgets/RegisterPage/register_indexes.dart';
 
 class RegisterPage extends StatelessWidget {
-  const RegisterPage({super.key});
+  final double textHeight;
+
+  const RegisterPage({
+    super.key,
+    this.textHeight = 15,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 50),
+
+              /// Header Text
+              const RegisterHeading(),
+              SizedBox(height: textHeight),
+
+              /// Username
+              const RegisterTextfield(hintText: "Username"),
+              SizedBox(height: textHeight),
+
+              /// Full Name
+              const RegisterTextfield(hintText: "Full Name"),
+              SizedBox(height: textHeight),
+
+              /// Email Address
+              const RegisterTextfield(hintText: "Email Address"),
+              SizedBox(height: textHeight),
+
+              /// Date Of Birth
+              const RegisterTextfield(hintText: "DOB-PLACEHOLDERTEST"),
+              SizedBox(height: textHeight),
+
+              /// Password
+              const RegisterTextfield(hintText: "Password", obscureText: true),
+              SizedBox(height: textHeight),
+
+              /// Retype Password
+              const RegisterTextfield(
+                  hintText: "Re-type Password", obscureText: true),
+              SizedBox(height: textHeight),
+
+              /// Divider
+              const RegisterDivider(),
+
+              /// Privacy Policy
+              PrivacyPolicyText(
+                onPrivacyPolicyTap: () {
+                  print("ROUTE THIS TO PRIVACY POLICY PAGE");
+                },
+              ),
+              SizedBox(height: textHeight),
+
+              /// Continue Button
+              RegisterButton(
+                onPressed: () {
+                  print("PLACEHOLDER, ROUTE TO NEXT PAGE");
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
